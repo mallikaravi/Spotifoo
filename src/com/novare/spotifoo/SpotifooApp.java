e39587f17ae977da3481befbdfa0d7076ff09c93
mallikaravi <mailtomallikaravi@gmail.com>
2022-07-14 22:30:35 +0200

    Refactored the code, resolved infinite loop

--- src/com/novare/spotifoo/SpotifooApp.java
+++ src/com/novare/spotifoo/SpotifooApp.java
@@ -1,7 +1,6 @@
 package com.novare.spotifoo;
 
 import java.awt.Desktop;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -18,17 +17,10 @@ public class SpotifooApp {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Spotifoo music player! \n\n");
 		Database.INST.readSongsData("assets/data.txt");
-
-		while (true) {
-			try {
-				mainMenu();
-			} catch (Exception e) {
-				System.out.println("Invalid option");
-			}
-		}
+		mainMenu();
 	}
 
-	private static void mainMenu() throws Exception {
+	private static void mainMenu() {
 		System.out.println("Main menu options:");
 		System.out.println("[1] Songs");
 		System.out.println("[2] Artists");
@@ -40,7 +32,10 @@ public class SpotifooApp {
 //		System.out.flush();
 
 		Scanner read = new Scanner(System.in);
-		int choice = read.nextInt();
+		int choice = 0;
+		if (read.hasNextInt()) {
+			choice = read.nextInt();
+		}
 		switch (choice) {
 		case 1: {
 			songsMenu(Database.INST.getAllSongs());
@@ -60,139 +55,115 @@ public class SpotifooApp {
 		}
 		case 5: {
 			searchMenu();
+			break;
 		}
 		default:
 			System.out.println("Invalid option");
+			mainMenu();
 		}
 		read.close();
-
 	}
 
-//	private static void clearScreen() {
-//		try {
-//			String[] cmd;
-//			// checking for OS
-//			if (System.getProperty("os.name").contains("Windows")) {
-//				cmd = new String[] { "cmd", "/c", "cls" };
-//			} else {
-//				cmd = new String[] { "clear" };
-//			}
-//			// clearing the screen
-//			ProcessBuilder pb = new ProcessBuilder(cmd);
-//			pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
-//			Process process = pb.start();
-//			process.waitFor();
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-
 	private static void songsMenu(List<Song> allSongs) {
-		try {
-			System.out.println("Songs menu:");
+		System.out.println("Songs menu:");
 
-			display(allSongs);
+		display(allSongs);
 
-			Scanner read = new Scanner(System.in);
-			int choice = read.nextInt();
-			switch (choice) {
-			case 0: {
-				mainMenu();
-				break;
-			}
-			default:
-				if (choice < allSongs.size()) {
-					Song song = allSongs.get(choice - 1);
-					play(song);
-					System.out.println("Playing file!");
-					System.exit(0);
-				} else {
-					System.out.println("Invalid option");
-				}
-			}
-			read.close();
-		} catch (Exception e) {
-			System.out.println("Invalid option");
+		Scanner read = new Scanner(System.in);
+		int choice = -1;
+		if (read.hasNextInt()) {
+			choice = read.nextInt();
 		}
+		if (choice == 0) {
+			mainMenu();
+		} else if (choice > 0 && choice <= allSongs.size()) {
+			Song song = allSongs.get(choice - 1);
+			play(song);
+			System.out.println("Playing file!");
+			System.exit(0);
+		} else {
+			System.out.println("Invalid option");
+			songsMenu(allSongs);
+		}
+		read.close();
 	}
 
 	private static void albumMenu(List<Album> allAlbums) {
-		try {
-			System.out.println("Songs menu:");
-			display(allAlbums);
+		System.out.println("album available:");
+		display(allAlbums);
 
-			Scanner read = new Scanner(System.in);
-			int choice = read.nextInt();
-			switch (choice) {
-			case 0: {
-				mainMenu();
-				break;
-			}
-			default:
-				if (choice < allAlbums.size()) {
-					Album album = allAlbums.get(choice - 1);
-					songsMenu(album.getSongs());
-				} else {
-					System.out.println("Invalid option");
-				}
-			}
-			read.close();
-		} catch (Exception e) {
-			System.out.println("Invalid option");
+		Scanner read = new Scanner(System.in);
+		int choice = -1;
+		if (read.hasNextInt()) {
+			choice = read.nextInt();
 		}
+		switch (choice) {
+		case 0: {
+			mainMenu();
+			break;
+		}
+		default:
+			if (choice > 0 && choice <= allAlbums.size()) {
+				Album album = allAlbums.get(choice - 1);
+				songsMenu(album.getSongs());
+			} else {
+				System.out.println("Invalid option");
+				albumMenu(allAlbums);
+			}
+		}
+		read.close();
 	}
 
 	private static void artistMenu(List<Artist> allArtist) {
-		try {
-			System.out.println("artist available:");
-			display(allArtist);
+		System.out.println("artist available:");
+		display(allArtist);
 
-			Scanner read = new Scanner(System.in);
-			int choice = read.nextInt();
-			switch (choice) {
-			case 0: {
-				mainMenu();
-				break;
-			}
-			default:
-				if (choice < allArtist.size()) {
-					Artist artist = allArtist.get(choice - 1);
-					songsMenu(artist.getSongs());
-				} else {
-					System.out.println("Invalid option");
-				}
-			}
-			read.close();
-		} catch (Exception e) {
-			System.out.println("Invalid option");
+		Scanner read = new Scanner(System.in);
+		int choice = -1;
+		if (read.hasNextInt()) {
+			choice = read.nextInt();
 		}
+		switch (choice) {
+		case 0: {
+			mainMenu();
+			break;
+		}
+		default:
+			if (choice > 0 && choice <= allArtist.size()) {
+				Artist artist = allArtist.get(choice - 1);
+				songsMenu(artist.getSongs());
+			} else {
+				System.out.println("Invalid option");
+				artistMenu(allArtist);
+			}
+		}
+		read.close();
 	}
 
 	private static void genreMenu(List<Genre> allGenres) {
-		try {
-			System.out.println("Songs menu:");
-			display(allGenres);
+		System.out.println("genre available:");
+		display(allGenres);
 
-			Scanner read = new Scanner(System.in);
-			int choice = read.nextInt();
-			switch (choice) {
-			case 0: {
-				mainMenu();
-				break;
-			}
-			default:
-				if (choice < allGenres.size()) {
-					Genre genre = allGenres.get(choice - 1);
-					songsMenu(genre.getSongs());
-				} else {
-					System.out.println("Invalid option");
-				}
-			}
-			read.close();
-		} catch (Exception e) {
-			System.out.println("Invalid option");
+		Scanner read = new Scanner(System.in);
+		int choice = -1;
+		if (read.hasNextInt()) {
+			choice = read.nextInt();
 		}
+		switch (choice) {
+		case 0: {
+			mainMenu();
+			break;
+		}
+		default:
+			if (choice > 0 && choice <= allGenres.size()) {
+				Genre genre = allGenres.get(choice - 1);
+				songsMenu(genre.getSongs());
+			} else {
+				System.out.println("Invalid option");
+				genreMenu(allGenres);
+			}
+		}
+		read.close();
 	}
 
 	private static void searchMenu() {
@@ -201,14 +172,13 @@ public class SpotifooApp {
 
 	private static void display(List<?> objects) {
 		for (int i = 0; i < objects.size(); i++) {
-			System.out.println("["+(i+1) +"] "+ objects.get(i));
-			
+			System.out.println("[" + (i + 1) + "] " + objects.get(i));
 		}
 		System.out.println("[0] Back to main menu");
 		System.out.print("Choose an option and press enter:");
 	}
 
-	private static void play(Song song) throws IOException {
+	private static void play(Song song) {
 		if (Desktop.isDesktopSupported()) {
 			try {
 				Desktop.getDesktop().open(Paths.get(song.getFileName()).toFile());
